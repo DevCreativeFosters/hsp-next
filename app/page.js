cd80af6f7d6f5cf04159a23903a76a454c40a02e
Marcin Krzemiński <marcin@krzeminski.net>
2023-06-28 20:35:29 +0200

    Display list of posts on the homepage

--- app/page.js
+++ app/page.js
@@ -1,6 +1,15 @@
-export default function Home() {
-  return (
-    <main>
-    </main>
-  )
+import {getAllPosts} from "@/lib/api";
+
+export default async function Home() {
+    const posts = await getAllPosts();
+    return (
+        <main>
+            <h1>Posts</h1>
+            <ul>
+                {posts.edges.map((post) => (
+                    <li>{post.node.title}</li>
+                ))}
+            </ul>
+        </main>
+    )
 }
