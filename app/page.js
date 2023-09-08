6c00bb4c93ec162a1f663c465dcdda5e0b190d15
Aleksander Windak <aleksander.windak@xfive.co>
2023-09-08 11:09:26 +0200

    Add Australia map as bg

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import { getPageData, getMenus, getGlobalOptions } from '@lib/api';
-import Layout from '@components/layout/layout';
 import { renderBlocks } from '@lib/block';
+import Background from '@components/background/background';
+import Layout from '@components/layout/layout';
 
 export const metadata = {
   title: 'HSP 4x4 - Homepage',
@@ -11,11 +12,16 @@ export default async function HomePage() {
   const content = await getPageData('');
   const globalOptions = await getGlobalOptions();
   const menus = await getMenus();
-
   return (
-    <Layout menus={menus} globalOptions={globalOptions}>
+    <Layout menus={menus} globalOptions={globalOptions} withMap>
       {content?.map(block => {
-        return renderBlocks(block);
+        const { background } = block;
+        const content = renderBlocks(block);
+        return background ? (
+          <Background colorStops={background}>{content}</Background>
+        ) : (
+          content
+        );
       })}
     </Layout>
   );
