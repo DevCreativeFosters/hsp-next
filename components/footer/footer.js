dca71c773ef93c93022eb21448647a5bacb1a020
Marti Belegu <marti.belegu@xfive.co>
2023-09-11 15:41:09 +0200

    HSP-20 Footer QA feedback

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,125 +1,186 @@
 import Button from '@components/button';
 import Logo from '@images/logo.svg';
 import styles from './footer.module.scss';
+import Container from '@components/container/container';
 
-export default function Footer({menus, text}) {
+export default function Footer({ menus, text }) {
   const year = new Date().getFullYear();
 
   return (
-    <div className={styles.footerWrapper}>
-      <footer className={styles.footer}>
-        <div className={styles.footerContainer}>
-          <div className={styles.column}>
-            <div className={styles.logo}>
-              <Logo/>
+    <div className={styles.gradientContainer}>
+      <Container>
+        <div className={styles.footerWrapper}>
+          <footer className={styles.footer}>
+            <div className={styles.footerContainer}>
+              <div className={styles.column}>
+                <div className={styles.logo}>
+                  <Logo />
+                </div>
+                <p className={styles.text}>{text}</p>
+              </div>
+              <div className={styles.column}>
+                <ul className={styles.menuList}>
+                  {menus.hsp.map((menu, idx) => (
+                    <li key={menu.label + idx} className={styles.menuItem}>
+                      {idx === 0 ? (
+                        <Button
+                          footer={true}
+                          href={menu.url}
+                          variant="tertiary"
+                          rightIcon="arrow-forward"
+                        >
+                          {menu.label}
+                        </Button>
+                      ) : (
+                        <Button
+                          href={menu.url}
+                          variant="footer-item"
+                          size="xsmall"
+                        >
+                          {menu.label}
+                        </Button>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+                <ul className={styles.menuList}>
+                  {menus.lifestyle.map((menu, idx) => (
+                    <li key={menu.label + idx} className={styles.menuItem}>
+                      {idx === 0 ? (
+                        <Button
+                          footer={true}
+                          href={menu.url}
+                          variant="tertiary"
+                          rightIcon="arrow-forward"
+                        >
+                          {menu.label}
+                        </Button>
+                      ) : (
+                        <Button
+                          href={menu.url}
+                          variant="footer-item"
+                          size="xsmall"
+                        >
+                          {menu.label}
+                        </Button>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className={styles.column}>
+                <ul className={styles.menuList}>
+                  {menus.products.map((menu, idx) => (
+                    <li key={menu.label + idx} className={styles.menuItem}>
+                      {idx === 0 ? (
+                        <Button
+                          footer={true}
+                          href={menu.url}
+                          variant="tertiary"
+                          rightIcon="arrow-forward"
+                        >
+                          {menu.label}
+                        </Button>
+                      ) : (
+                        <Button
+                          href={menu.url}
+                          variant="footer-item"
+                          size="xsmall"
+                        >
+                          {menu.label}
+                        </Button>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className={styles.column}>
+                <ul className={styles.menuList}>
+                  {menus.services.map((menu, idx) => (
+                    <li key={menu.label + idx} className={styles.menuItem}>
+                      {idx === 0 ? (
+                        <Button
+                          footer={true}
+                          href={menu.url}
+                          variant="tertiary"
+                          rightIcon="arrow-forward"
+                        >
+                          {menu.label}
+                        </Button>
+                      ) : (
+                        <Button
+                          href={menu.url}
+                          variant="footer-item"
+                          size="xsmall"
+                        >
+                          {menu.label}
+                        </Button>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className={styles.column}>
+                <ul className={styles.menuList}>
+                  {menus.resources.map((menu, idx) => (
+                    <li key={menu.label + idx} className={styles.menuItem}>
+                      {idx === 0 ? (
+                        <Button
+                          footer={true}
+                          href={menu.url}
+                          variant="tertiary"
+                          rightIcon="arrow-forward"
+                        >
+                          {menu.label}
+                        </Button>
+                      ) : (
+                        <Button
+                          href={menu.url}
+                          variant="footer-item"
+                          size="xsmall"
+                        >
+                          {menu.label}
+                        </Button>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+                <ul className={styles.menuList}>
+                  {menus.legal.map((menu, idx) => (
+                    <li key={menu.label + idx} className={styles.menuItem}>
+                      {idx === 0 ? (
+                        <Button
+                          footer={true}
+                          href={menu.url}
+                          variant="tertiary"
+                          rightIcon="arrow-forward"
+                        >
+                          {menu.label}
+                        </Button>
+                      ) : (
+                        <Button
+                          href={menu.url}
+                          variant="footer-item"
+                          size="xsmall"
+                        >
+                          {menu.label}
+                        </Button>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
-            <p className={styles.text}>{text}</p>
-          </div>
-          <div className={styles.column}>
-            <ul className={styles.menuList}>
-              {menus.hsp.map((menu, idx) => (
-                <li key={menu.label + idx} className={styles.menuItem}>
-                  {idx === 0 ?
-                    <Button footer={true}
-                      href={menu.url}
-                      variant='tertiary'
-                      rightIcon='arrow-forward'>
-                        {menu.label}
-                    </Button> :
-                    <Button href={menu.url} variant="footer-item" size="xsmall">{menu.label}</Button>
-                  }
-                </li>
-              ))}
-            </ul>
-            <ul className={styles.menuList}>
-              {menus.lifestyle.map((menu, idx) => (
-              <li key={menu.label + idx} className={styles.menuItem}>
-                  {idx === 0 ?
-                    <Button footer={true}
-                      href={menu.url}
-                      variant='tertiary'
-                      rightIcon='arrow-forward'>
-                        {menu.label}
-                    </Button> :
-                    <Button href={menu.url} variant="footer-item" size="xsmall">{menu.label}</Button>
-                  }
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className={styles.column}>
-            <ul className={styles.menuList}>
-              {menus.products.map((menu, idx) => (
-              <li key={menu.label + idx} className={styles.menuItem}>
-                  {idx === 0 ?
-                    <Button footer={true}
-                      href={menu.url}
-                      variant='tertiary'
-                      rightIcon='arrow-forward'>
-                        {menu.label}
-                    </Button> :
-                    <Button href={menu.url} variant="footer-item" size="xsmall">{menu.label}</Button>
-                  }
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className={styles.column}>
-            <ul className={styles.menuList}>
-              {menus.services.map((menu, idx) => (
-              <li key={menu.label + idx} className={styles.menuItem}>
-                  {idx === 0 ?
-                    <Button footer={true}
-                      href={menu.url}
-                      variant='tertiary'
-                      rightIcon='arrow-forward'>
-                        {menu.label}
-                    </Button> :
-                    <Button href={menu.url} variant="footer-item" size="xsmall">{menu.label}</Button>
-                  }
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className={styles.column}>
-            <ul className={styles.menuList}>
-              {menus.resources.map((menu, idx) => (
-              <li key={menu.label + idx} className={styles.menuItem}>
-                  {idx === 0 ?
-                    <Button footer={true}
-                      href={menu.url}
-                      variant='tertiary'
-                      rightIcon='arrow-forward'>
-                        {menu.label}
-                    </Button> :
-                    <Button href={menu.url} variant="footer-item" size="xsmall">{menu.label}</Button>
-                  }
-                </li>
-              ))}
-            </ul>
-            <ul className={styles.menuList}>
-              {menus.legal.map((menu, idx) => (
-              <li key={menu.label + idx} className={styles.menuItem}>
-                  {idx === 0 ?
-                    <Button footer={true}
-                      href={menu.url}
-                      variant='tertiary'
-                      rightIcon='arrow-forward'>
-                        {menu.label}
-                    </Button> :
-                    <Button href={menu.url} variant="footer-item" size="xsmall">{menu.label}</Button>
-                  }
-                </li>
-              ))}
-            </ul>
-          </div>
+            <div className={styles.copyright}>
+              <p className={styles.copyrightText}>
+                Copyright HSP UTE LIDS {year}
+              </p>
+              <p className={styles.copyrightText}>Made by Xfive</p>
+            </div>
+          </footer>
         </div>
-        <div className={styles.copyright}>
-          <p className={styles.copyrightText}>Copyright HSP UTE LIDS {year}</p>
-          <p className={styles.copyrightText}>Made by Xfive</p>
-        </div>
-      </footer>
-      <div className={styles.backgroundGradient}/>
+      </Container>
+      <div className={styles.backgroundGradient} />
     </div>
-  )
+  );
 }
