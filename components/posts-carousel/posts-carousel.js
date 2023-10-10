ade9d63612e61abe420e3004a1fc27d019bc03b2
Marti Belegu <marti.belegu@xfive.co>
2023-10-10 14:48:51 +0200

    HSP-121 hsp tv page qa feedback

--- components/posts-carousel/posts-carousel.js
+++ components/posts-carousel/posts-carousel.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useCallback, useRef } from 'react';
+import { useCallback, useRef, useEffect, useMemo } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import Tile from '@components/tile/tile';
 import Container from '@components/container/container';
@@ -14,15 +14,36 @@ export default function PostsCarousel({ title, description, button, posts }) {
   let swiperRef = useRef(null);
 
   const groupSize = isMobile ? 1 : 3;
-  const groupedPosts = posts
-    ? posts.reduce((acc, post, index) => {
-        if (index % groupSize === 0) {
-          acc.push([]);
-        }
-        acc[acc.length - 1].push(post);
-        return acc;
-      }, [])
-    : [];
+  const groupedPosts = useMemo(() => {
+    return posts
+      ? posts.reduce((acc, post, index) => {
+          if (index % groupSize === 0) {
+            acc.push([]);
+          }
+          acc[acc.length - 1].push(post);
+          return acc;
+        }, [])
+      : [];
+  }, [posts, groupSize]);
+
+  useEffect(
+    function makeSlidesSameHeight() {
+      if (swiperRef.current) {
+        let maxHeight = 0;
+        swiperRef.current.slides.forEach(slide => {
+          const slideHeight = slide.offsetHeight;
+          maxHeight = Math.max(maxHeight, slideHeight);
+        });
+
+        swiperRef.current.slides.forEach(slide => {
+          slide.style.height = `${maxHeight}px`;
+        });
+
+        swiperRef.current.update();
+      }
+    },
+    [groupedPosts, isMobile],
+  );
 
   const handlePrevClick = useCallback(() => {
     if (swiperRef.current) {
@@ -40,9 +61,11 @@ export default function PostsCarousel({ title, description, button, posts }) {
     <Container>
       <div className={styles.container}>
         <div className={styles.sectionInformation}>
-          <h2 className={styles.title}>{title}</h2>
-          <div className={styles.text}>
-            {description && <p>{description}</p>}
+          <div className={styles.header}>
+            <h2 className={styles.title}>{title}</h2>
+            <div className={styles.text}>
+              {description && <p>{description}</p>}
+            </div>
           </div>
           <div className={styles.actionButtons}>
             {groupedPosts.length > 1 && (
@@ -59,11 +82,7 @@ export default function PostsCarousel({ title, description, button, posts }) {
                 />
               </div>
             )}
-            <Button
-              href={button.url}
-              variant={button.variant}
-              rightIcon="external-link"
-            >
+            <Button href={button.url} variant={button.variant}>
               {button.title}
             </Button>
           </div>
