2bbf60d8cac63c7441ac92dd19c287f65b3019e5
Aleksander Windak <aleksander.windak@xfive.co>
2023-10-13 12:34:41 +0200

    Add marker clusterer

--- components/store-locator-search/store-locator-results-and-map.js
+++ components/store-locator-search/store-locator-results-and-map.js
@@ -3,6 +3,7 @@
 import { useContext, useEffect, useState } from 'react';
 import { Loader } from '@googlemaps/js-api-loader';
 import { computeDistanceBetween } from 'spherical-geometry-js';
+import { MarkerClusterer, MarkerUtils } from '@googlemaps/markerclusterer';
 import StoreLocatorContext from '@contexts/store-locator';
 import Container from '@components/container/container';
 import StoreLocatorResultItem from '@components/store-locator-search/store-locator-result-item';
@@ -80,9 +81,10 @@ export default function StoreLocatorResultsAndMap() {
 
       const map = new Map(document.getElementById('store-locator-map'), {
         center: { lat: -26, lng: 133 },
-        // center: { lat: 0, lng: 0 },
         zoom: 4,
         mapId: NEXT_PUBLIC_GOOGLE_MAPS_MAP_ID,
+        minZoom: 2,
+        maxZoom: 17,
         mapTypeControl: false,
         // streetViewControl: false,
       });
@@ -95,14 +97,60 @@ export default function StoreLocatorResultsAndMap() {
     function syncMapMarkers() {
       let markers = [];
       if (googleMap) {
+        const bounds = new google.maps.LatLngBounds();
         markers = filteredLocations.map(({ name, geolocation, icon }) => {
           const marker = new google.maps.Marker({
             position: geolocation,
             title: name,
+            // map: googleMap,
             icon,
           });
-          marker.setMap(googleMap);
+          bounds.extend(geolocation);
           return marker;
+        });
+        googleMap.fitBounds(bounds);
+
+        new MarkerClusterer({
+          map: googleMap,
+          markers,
+          renderer: {
+            render: ({ count, position }, stats, map) => {
+              const svg = `
+              <svg fill="#ed2935" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 240 240" width="50" height="50">
+                <circle cx="120" cy="120" opacity=".8" r="70" />
+                <circle cx="120" cy="120" opacity=".4" r="90" />
+                <circle cx="120" cy="120" opacity=".2" r="110" />
+                <text x="50%" y="50%" style="fill:#fff" text-anchor="middle" font-size="50" dominant-baseline="middle" font-family="roboto,arial,sans-serif">${count}</text>
+              </svg>`;
+              const title = `Cluster of ${count} markers`;
+              const zIndex = Number(google.maps.Marker.MAX_ZINDEX) + count;
+              if (MarkerUtils.isAdvancedMarkerAvailable(map)) {
+                const parser = new DOMParser();
+                const svgEl = parser.parseFromString(
+                  svg,
+                  'image/svg+xml',
+                ).documentElement;
+                svgEl.setAttribute('transform', 'translate(0 25)');
+                return new google.maps.marker.AdvancedMarkerElement({
+                  map,
+                  position,
+                  zIndex,
+                  title,
+                  content: svgEl,
+                });
+              } else {
+                return new google.maps.Marker({
+                  position,
+                  zIndex,
+                  title,
+                  icon: {
+                    url: `data:image/svg+xml;base64,${btoa(svg)}`,
+                    anchor: new google.maps.Point(25, 25),
+                  },
+                });
+              }
+            },
+          },
         });
       }
 
