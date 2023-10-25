8b11d5c9ecfe2558e3472256ee32d4e0cf31ef41
marekxhtml <marek.klementa@xfive.co>
2023-10-25 14:29:31 +0200

    [HSP-164] Fix for the make page

--- app/products/[mainCategorySlug]/[makeSlug]/page.js
+++ app/products/[mainCategorySlug]/[makeSlug]/page.js
@@ -8,7 +8,7 @@ export default async function CategoryPage({ params }) {
   const makeSlug = params.makeSlug;
   const makeData = await getMake(makeSlug);
   const details = makeData.detailsFields.details;
-  const filteredData = details.filter(
+  const filteredData = details?.filter(
     data => data.relatedProductCategory.slug === mainCategorySlug,
   );
 
@@ -16,7 +16,7 @@ export default async function CategoryPage({ params }) {
     <Layout title="Product">
       <Container>
         <h2>{makeData.name}</h2>
-        {filteredData.featuredImage && (
+        {filteredData?.featuredImage && (
           <Image
             src={filteredData.featuredImage.mediaItemUrl}
             width={filteredData.featuredImage.mediaDetails.width}
@@ -26,16 +26,16 @@ export default async function CategoryPage({ params }) {
         )}
         <div>
           Features:{' '}
-          {filteredData.features && (
+          {filteredData?.features && (
             <div dangerouslySetInnerHTML={{ __html: filteredData?.features }} />
           )}
         </div>
-        <div>From Price: {filteredData.fromPrice}</div>
+        <div>From Price: {filteredData?.fromPrice}</div>
         <div>
-          Warranty Time Period: {filteredData.warranty?.warrantyTimePeriod}
+          Warranty Time Period: {filteredData?.warranty?.warrantyTimePeriod}
         </div>
         <div>
-          Warranty Description: {filteredData.warranty?.warrantyDescription}
+          Warranty Description: {filteredData?.warranty?.warrantyDescription}
         </div>
       </Container>
     </Layout>
