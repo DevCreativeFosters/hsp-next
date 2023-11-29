813cfaee09390deab8f7e2f802927c99c1df4e63
Marti Belegu <marti.belegu@xfive.co>
2023-11-29 10:30:47 +0100

    HSP-210 blog post and hsp tv post sidebar jumping issue

--- components/related-posts/related-posts.js
+++ components/related-posts/related-posts.js
@@ -1,11 +1,10 @@
 import Link from 'next/link';
+import routes from '@lib/routes';
 import Button from '@components/button/button';
 import styles from './related-posts.module.scss';
 
 export default function RelatedPosts({ type = 'hsp_tv', posts, url }) {
   const title = type === 'blog' ? 'HSP Blog' : type === 'hsp-tv' && 'HSP TV';
-  const parentSlug =
-    type === 'blog' ? '/hsp-blog' : type === 'hsp-tv' && '/hsp-tv';
 
   return (
     <div className={styles.relatedPosts}>
@@ -21,13 +20,26 @@ export default function RelatedPosts({ type = 'hsp_tv', posts, url }) {
         </Button>
       )}
       <ul className={styles.list}>
-        {posts.map(post => (
-          <li key={post?.uri}>
-            <Link className={styles.link} href={`${parentSlug}/${post?.uri}`}>
-              {post?.title}
-            </Link>
-          </li>
-        ))}
+        {posts.map(post => {
+          const parts = post.uri.split('/');
+          const slug = parts[parts.length - 2];
+          return (
+            <li key={slug}>
+              <Link
+                className={styles.link}
+                href={
+                  type === 'blog'
+                    ? routes.blog(slug)
+                    : type === 'hsp-tv'
+                    ? routes.tv(slug)
+                    : '#'
+                }
+              >
+                {post?.title}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
