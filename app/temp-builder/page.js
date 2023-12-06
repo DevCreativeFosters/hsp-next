7400ba6521fec1be2b677c63aaacaae65ebb99b6
Mateusz Czpak <mateusz.czpak@xfive.co>
2023-12-06 19:23:02 +0100

    Product carousel

--- app/temp-builder/page.js
+++ app/temp-builder/page.js
@@ -1,22 +1,42 @@
-// import { getPageData } from '@lib/api';
+import { getProductsWithVariants } from '@lib/api';
 import Layout from '@components/layout/layout';
 import Container from '@components/container/container';
 import ProductsCarousel from '@components/builder/products-carousel';
+
+import { default as productsMock } from '@components/builder/mock-data';
 
 export const metadata = {
   title: 'HSP 4x4 - UTP Builder',
   // description: ''
 };
 
-import products from '@components/builder/mock-data';
+const prodTemp = [];
 
 export default async function TempBuilderPage() {
-  // const content = await getPageData('');
+  const products = await getProductsWithVariants(
+    'ford',
+    'next-gen-ranger-raptor',
+  );
+  const productsVariants = [];
+
+  productsMock?.forEach(product => {
+    if (product.productFields.variants) {
+      product.productFields.variants.forEach(productVariant => {
+        productsVariants.push({
+          ...productVariant,
+          price: productVariant.variantDetails.price,
+          installationCost:
+            productVariant.installationCost ||
+            product.productFields.installationCost,
+        });
+      });
+    }
+  });
 
   return (
     <Layout>
       <Container>
-        <ProductsCarousel products={products} />
+        <ProductsCarousel products={productsVariants} />
       </Container>
     </Layout>
   );
--- components/builder/mock-data.js
+++ components/builder/mock-data.js
@@ -1,51 +1,173 @@
 const products = [
   {
-    sourceUrl:
-      'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/b8c21b096159073d252075526d48dbcd-scaled.jpeg',
-    alt: 'mainImage',
-    mainImage: true,
+    productFields: {
+      description: null,
+      price: null,
+      installationCost: null,
+      variants: null,
+    },
   },
   {
-    sourceUrl:
-      'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/lid-scaled.jpeg',
-    alt: 'mainImage',
-    mainImage: false,
-  },
-  {
-    sourceUrl:
-      'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/lid-scaled.jpeg',
-    alt: 'mainImage',
-    mainImage: false,
-  },
-  {
-    sourceUrl:
-      'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/lid-scaled.jpeg',
-    alt: 'mainImage',
-    mainImage: false,
-  },
-  {
-    sourceUrl:
-      'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/lid-scaled.jpeg',
-    alt: 'mainImage',
-    mainImage: false,
-  },
-  {
-    sourceUrl:
-      'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/lid-scaled.jpeg',
-    alt: 'mainImage',
-    mainImage: false,
-  },
-  {
-    sourceUrl:
-      'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/lid-scaled.jpeg',
-    alt: 'mainImage',
-    mainImage: false,
-  },
-  {
-    sourceUrl:
-      'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/lid-scaled.jpeg',
-    alt: 'mainImage',
-    mainImage: false,
+    productFields: {
+      description:
+        'We have transformed the traditional hard lid to a better functioning, better priced, better suited product worthy of your vehicle.',
+      price: 1000,
+      installationCost: null,
+      variants: [
+        {
+          parentInherit: true,
+          variantSlug: '/1',
+          variantName: 'Premium Hard Lid 1',
+          uteBuilderImages: {
+            imageDesktop: {
+              sourceUrl:
+                'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/lid-scaled.jpeg',
+            },
+            imageMobile: {
+              sourceUrl:
+                'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/lid-scaled.jpeg',
+            },
+          },
+          variantDetails: {
+            price: null,
+            installationCost: null,
+          },
+        },
+        {
+          parentInherit: true,
+          variantSlug: '/4',
+          variantName: 'Silverback Lid 2',
+          uteBuilderImages: {
+            imageDesktop: {
+              sourceUrl:
+                'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/lid-scaled.jpeg',
+            },
+            imageMobile: {
+              sourceUrl:
+                'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/lid-scaled.jpeg',
+            },
+          },
+          variantDetails: {
+            price: null,
+            installationCost: null,
+          },
+        },
+        {
+          parentInherit: true,
+          variantSlug: '/4',
+          variantName: 'Silverback Lid 3',
+          uteBuilderImages: {
+            imageDesktop: {
+              sourceUrl:
+                'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/lid-scaled.jpeg',
+            },
+            imageMobile: {
+              sourceUrl:
+                'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/lid-scaled.jpeg',
+            },
+          },
+          variantDetails: {
+            price: null,
+            installationCost: null,
+          },
+        },
+        {
+          parentInherit: true,
+          variantSlug: '/5',
+          variantName: 'Silverback Lid 4',
+          uteBuilderImages: {
+            imageDesktop: {
+              sourceUrl:
+                'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/lid-scaled.jpeg',
+            },
+            imageMobile: {
+              sourceUrl:
+                'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/lid-scaled.jpeg',
+            },
+          },
+          variantDetails: {
+            price: null,
+            installationCost: null,
+          },
+        },
+        {
+          parentInherit: true,
+          variantSlug: '/5',
+          variantName: 'Silverback Lid 5',
+          uteBuilderImages: {
+            imageDesktop: {
+              sourceUrl:
+                'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/lid-scaled.jpeg',
+            },
+            imageMobile: {
+              sourceUrl:
+                'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/lid-scaled.jpeg',
+            },
+          },
+          variantDetails: {
+            price: null,
+            installationCost: null,
+          },
+        },
+        {
+          parentInherit: true,
+          variantSlug: '/5',
+          variantName: 'Silverback Lid 6',
+          uteBuilderImages: {
+            imageDesktop: {
+              sourceUrl:
+                'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/lid-scaled.jpeg',
+            },
+            imageMobile: {
+              sourceUrl:
+                'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/lid-scaled.jpeg',
+            },
+          },
+          variantDetails: {
+            price: null,
+            installationCost: null,
+          },
+        },
+        {
+          parentInherit: true,
+          variantSlug: '/5',
+          variantName: 'Silverback Lid 7',
+          uteBuilderImages: {
+            imageDesktop: {
+              sourceUrl:
+                'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/lid-scaled.jpeg',
+            },
+            imageMobile: {
+              sourceUrl:
+                'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/lid-scaled.jpeg',
+            },
+          },
+          variantDetails: {
+            price: null,
+            installationCost: null,
+          },
+        },
+        {
+          parentInherit: true,
+          variantSlug: '/5',
+          variantName: 'Silverback Lid 8',
+          uteBuilderImages: {
+            imageDesktop: {
+              sourceUrl:
+                'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/lid-scaled.jpeg',
+            },
+            imageMobile: {
+              sourceUrl:
+                'http://hsp-wp.x5view.co/wp-content/uploads/2023/08/lid-scaled.jpeg',
+            },
+          },
+          variantDetails: {
+            price: null,
+            installationCost: null,
+          },
+        },
+      ],
+    },
   },
 ];
 
