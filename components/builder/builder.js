22e11e35762f0b3ffea15683d12048cb3727fc0f
Mateusz Czpak <mateusz.czpak@xfive.co>
2023-12-08 16:04:45 +0100

    Sidebar in progress

--- components/builder/builder.js
+++ components/builder/builder.js
@@ -12,24 +12,43 @@ export default function Builder({ model, products }) {
   const [selectedProducts, setSelectedProducts] = useState([]);
   const [disabledProducts, setDisabledProducts] = useState([]);
 
-  const toggleProduct = useCallback(
+  const addProduct = useCallback(
     product => {
-      const newSelectedProducts = selectedProducts.includes(product)
-        ? selectedProducts.filter(
-            selectedProduct => selectedProduct !== product,
-          )
-        : [...selectedProducts, product];
+      const newSelectedProducts = [...selectedProducts, product];
 
       setSelectedProducts(newSelectedProducts);
     },
     [selectedProducts],
   );
 
+  const removeProduct = useCallback(
+    product => {
+      const newSelectedProducts = selectedProducts.filter(
+        selectedProduct => selectedProduct !== product,
+      );
+
+      setSelectedProducts(newSelectedProducts);
+    },
+    [selectedProducts],
+  );
+
+  const toggleProduct = useCallback(
+    product => {
+      selectedProducts.includes(product)
+        ? removeProduct(product)
+        : addProduct(product);
+    },
+    [selectedProducts, addProduct, removeProduct],
+  );
+
   return (
     <div className={styles.builder}>
       <Container className={styles.container}>
         <div className={styles.top}>
-          <Sidebar selectedProducts={selectedProducts} />
+          <Sidebar
+            selectedProducts={selectedProducts}
+            removeProduct={removeProduct}
+          />
 
           <Preview model={model} selectedProducts={selectedProducts} />
         </div>
--- components/builder/sidebar.js
+++ components/builder/sidebar.js
@@ -1,8 +1,172 @@
 'use client';
 
+import { useState, useCallback, useEffect } from 'react';
 import clsx from 'clsx';
+
+import Button from '@components/button/button';
+import { getIcon } from '@lib/icons';
+import { formatPrice } from '@lib/helpers';
+
 import styles from './sidebar.module.scss';
 
-export default function Sidebar({ selectedProducts, className }) {
-  return <div className={clsx(styles.sidebar, className)}>Sidebar</div>;
+const ExpandIcon = getIcon('expand-more-neutral');
+const ListIcon = getIcon('list');
+const CancelIcon = getIcon('cancel');
+
+const DEFAULT_PRICE_SUMMARY = {
+  price: 0,
+  installationCost: 0,
+};
+
+const Section = ({
+  id,
+  headerChildren,
+  headerClick,
+  isOpen = false,
+  children,
+}) => {
+  return (
+    <div
+      className={clsx(styles.section, {
+        [styles.isOpen]: isOpen,
+      })}
+    >
+      <button
+        className={styles.sectionHeader}
+        type="button"
+        onClick={() => headerClick(id)}
+      >
+        {headerChildren}
+        <ExpandIcon className={styles.sectionHeaderIcon} />
+      </button>
+      <div className={styles.sectionContent}>{children}</div>
+    </div>
+  );
+};
+
+const ProductsList = ({ selectedProducts, removeProduct }) => {
+  return (
+    <>
+      {selectedProducts.length !== 0 ? (
+        <ol className={styles.productsList}>
+          {selectedProducts?.map(selectedProduct => {
+            const productTitle = selectedProduct.variantName;
+            const productPrice = selectedProduct.price;
+            const productSlug = selectedProduct.variantSlug;
+
+            return (
+              <li className={styles.productsListItem} key={productSlug}>
+                <div className={styles.productBox}>
+                  {formatPrice(productPrice)}
+                  <span className={styles.productBoxName}>{productTitle}</span>
+                </div>
+                <Button
+                  className={styles.productRemove}
+                  variant="quaternary"
+                  onClick={() => removeProduct(selectedProduct)}
+                >
+                  <CancelIcon />
+                </Button>
+              </li>
+            );
+          })}
+        </ol>
+      ) : (
+        <div className={styles.noProducts}>
+          Select products from the bottom of the screen to create a quote.
+          <div className={clsx(styles.productBox, styles.isEmpty)}>
+            No products added yet
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default function Sidebar({
+  selectedProducts,
+  removeProduct,
+  className,
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [openSection, setOpenSection] = useState('products');
+  const [priceSummary, setPriceSummary] = useState(DEFAULT_PRICE_SUMMARY);
+  const [store, setStore] = useState(null);
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen(!isOpen);
+  }, [isOpen]);
+
+  useEffect(
+    function calculatePrice() {
+      let newPriceSummary = selectedProducts.reduce(
+        (accumulator, currentProduct) => ({
+          price: accumulator.price + currentProduct.price,
+          installationCost:
+            accumulator.installationCost + currentProduct.installationCost,
+        }),
+        DEFAULT_PRICE_SUMMARY,
+      );
+
+      setPriceSummary(newPriceSummary);
+    },
+    [selectedProducts],
+  );
+
+  return (
+    <>
+      <div
+        className={clsx(styles.sidebar, className, {
+          [styles.isOpen]: isOpen,
+        })}
+      >
+        <Section
+          id="products"
+          headerChildren={<>Your setup</>}
+          headerClick={setOpenSection}
+          isOpen={openSection === 'products'}
+        >
+          <ProductsList
+            selectedProducts={selectedProducts}
+            removeProduct={removeProduct}
+          />
+        </Section>
+        <Section
+          id="store"
+          headerChildren={<>Locate your store</>}
+          headerClick={setOpenSection}
+          isOpen={openSection === 'store'}
+        >
+          Locator placeholder
+        </Section>
+        <div className={styles.summary}>
+          <div className={styles.summaryPrice}>
+            {formatPrice(priceSummary.price)}
+          </div>
+          <div className={styles.summaryInstallation}>
+            +{' '}
+            {priceSummary.installationCost === 0
+              ? 'installation cost'
+              : formatPrice(priceSummary.installationCost)}
+          </div>
+          <Button className={styles.summaryButton} size="large" disabled>
+            Send enquiry
+          </Button>
+        </div>
+      </div>
+      <div className={styles.sidebarMobileBar}>
+        <button
+          className={styles.sidebarMobileBarToggle}
+          onClick={toggleOpen}
+          type="button"
+        >
+          <ListIcon />
+        </button>
+        <div className={styles.sidebarMobileBarPrice}>
+          {formatPrice(priceSummary.price)}
+        </div>
+        <Button disabled>Send enquiry</Button>
+      </div>
+    </>
+  );
 }
