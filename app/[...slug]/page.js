5e455816fb230242188c30c03184b6bd91896cf6
marekxhtml <marek.klementa@xfive.co>
2023-12-18 14:11:04 +0100

    [HSP-260] Added dynamic route for the dynamic pages

---
+++ app/[...slug]/page.js
@@ -0,0 +1,23 @@
+import { notFound } from 'next/navigation';
+import { getPageData } from '@lib/api';
+import { renderBlock } from '@lib/block';
+import Layout from '@components/layout/layout';
+
+export async function generateMetadata({ params }) {
+  const content = await getPageData(params?.slug);
+
+  return {
+    title: `HSP 4x4 - ${content?.title}`,
+  };
+}
+
+export default async function HomePage({ params }) {
+  const content = await getPageData(params?.slug);
+  const contentBlocks = content?.flexibleContent?.blocks?.map(renderBlock);
+
+  if (!content) return notFound();
+
+  return (
+    <Layout withMap>{contentBlocks.map(contentBlock => contentBlock)}</Layout>
+  );
+}
