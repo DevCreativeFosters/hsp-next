da42c82805ddd07bd11f78f73f195ada06711a50
Marcin Krzemiński <marcin@krzeminski.net>
2024-02-20 18:33:53 +0100

    Fetch factory options

--- app/ute-builder/page.js
+++ app/ute-builder/page.js
@@ -3,6 +3,7 @@ import { getAllMakes } from '@lib/api/get-all-makes';
 import { getGlobalOptions } from '@lib/api/get-global-options';
 import { getTermChildren } from '@lib/api/get-term-children';
 import { StoreLocatorProvider } from '@contexts/store-locator';
+
 import Layout from '@components/layout/layout';
 import UteBuilderPage from '@components/ute-builder-page/ute-builder-page';
 
