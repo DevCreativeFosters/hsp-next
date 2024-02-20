6c84fe27cdcaa61e829a5531fe3c439778e9d67a
Filip Chrapek <hello@filipchrapek.com>
2024-02-20 10:31:18 +0100

    Add Instagram tile & icons

--- components/instagram-feed/instagram-feed.js
+++ components/instagram-feed/instagram-feed.js
@@ -4,14 +4,21 @@ import { useEffect, useState } from 'react';
 import Container from '@components/container/container';
 import TileCarousel from '@components/tile-carousel/tile-carousel';
 import SectionIntro from '@components/section-intro/section-intro';
-import Tile from '@components/tile/tile';
+import InstagramTile from '@components/instagram-tile/instagram-tile';
+import InstagramFeedSMLinks from './instagram-feed-sm-links';
+import Loading from '@components/loading/loading';
+import { getGlobalOptions } from '@lib/api';
+import normalizeSocialMediaMenu from '@lib/normalize-social-media-menu';
+import Button from '@components/button/button';
 
 const IG_USER_ID = process.env.NEXT_PUBLIC_IG_USER_ID;
 const IG_TOKEN = process.env.NEXT_PUBLIC_IG_TOKEN;
-const IG_URL = `https://graph.instagram.com/${IG_USER_ID}/media?access_token=${IG_TOKEN}&fields=media_url,permalink`;
+const IG_URL = `https://graph.instagram.com/${IG_USER_ID}/media?access_token=${IG_TOKEN}&fields=media_type,media_url,thumbnail_url,permalink`;
 
 export default function InstagramFeed({ title, description }) {
   const [igFeed, setIgFeed] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [socialMedia, setSocialMedia] = useState([]);
 
   useEffect(() => {
     const fetchMedia = async () => {
@@ -20,9 +27,11 @@ export default function InstagramFeed({ title, description }) {
         if (!res.ok) {
           throw new Error(`Error: ${res.status}`);
         }
+        setLoading(false);
         const json = await res.json();
         setIgFeed(json.data);
       } catch (error) {
+        setLoading(false);
         console.error('Failed to fetch Instagram feed:', error);
       }
     };
@@ -30,12 +39,25 @@ export default function InstagramFeed({ title, description }) {
     fetchMedia();
   }, []);
 
+  useEffect(() => {
+    const fetchSocialMedia = async () => {
+      try {
+        const globalOptions = await getGlobalOptions();
+        const socialData = normalizeSocialMediaMenu(globalOptions);
+        setSocialMedia(socialData);
+      } catch (error) {
+        console.error('Failed to fetch social media:', error);
+      }
+    };
+
+    fetchSocialMedia();
+  }, []);
+
   const transformedIgFeed = igFeed.map(post => ({
-    url: post.permalink,
-    image: {
-      sourceUrl: post.media_url,
-    },
-    variant: '',
+    slug: post.permalink,
+    type: post.media_type,
+    url: post.media_url,
+    thumbnailUrl: post.thumbnail_url,
   }));
 
   return (
@@ -46,7 +68,29 @@ export default function InstagramFeed({ title, description }) {
         fitInline
         narrowDescription
       />
-      <TileCarousel items={transformedIgFeed} itemTemplate={Tile} />
+
+      <ul>
+        {socialMedia.map(({ url, iconPredefined, icon }, index) => (
+          <li key={url + index}>
+            <Button
+              href={url}
+              size="small"
+              variant="tertiary"
+              background="dark"
+              leftIcon={iconPredefined !== 'CUSTOM' ? iconPredefined : false}
+              leftIconUrl={iconPredefined === 'CUSTOM' ? icon : false}
+            />
+          </li>
+        ))}
+      </ul>
+
+      {isLoading && (
+        <div>
+          <Loading color="white" size="large" />
+        </div>
+      )}
+
+      <TileCarousel items={transformedIgFeed} itemTemplate={InstagramTile} />
     </Container>
   );
 }
