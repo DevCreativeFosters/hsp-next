fcacfcc979cbdabb69d22496265eb49fbc99ec96
Marcin Krzemiński <marcin@krzeminski.net>
2024-04-09 16:39:37 +0200

    Provide setFactoryOption with vehicle context

--- contexts/vehicle.js
+++ contexts/vehicle.js
@@ -102,6 +102,7 @@ export const VehicleProvider = ({ children }) => {
         setDropdownOpened,
         handleVehicleReset,
         handleSave,
+        factoryOption,
         setFactoryOption,
       }}
     >
