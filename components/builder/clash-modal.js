fbf65ed614c27680e94814f015220153ed2dfc4d
Marcin Krzemiński <marcin@krzeminski.net>
2024-04-09 22:12:23 +0200

    Extract clash modal to a separate component

---
+++ components/builder/clash-modal.js
@@ -0,0 +1,55 @@
+import styles from '@components/builder/builder.module.scss';
+import Button from '@components/button/button';
+import Modal from '@components/modal/modal';
+
+export default function ClashModal({
+  setShowModal,
+  factoryOption,
+  currentProduct,
+  setFactoryOption,
+  setSelectedProducts,
+}) {
+  return (
+    <Modal
+      title="Factory Options Clash"
+      isVisible={true}
+      maxWidth={900}
+      onClose={() => {
+        setShowModal(false);
+      }}
+    >
+      <p>
+        For this selected HSP product to be installed, it requires the below
+        factory options to be removed.
+      </p>
+
+      <ol className={styles.list}>
+        <li>
+          <span className={styles.listItem}>{factoryOption.name}</span>
+        </li>
+      </ol>
+      <p>Do you want to proceed?</p>
+      <div className={styles.buttons}>
+        <Button
+          variant={'secondary'}
+          size={'large'}
+          onClick={() => {
+            setShowModal(false);
+          }}
+        >
+          Cancel
+        </Button>
+        <Button
+          size={'large'}
+          onClick={() => {
+            setFactoryOption(null);
+            setSelectedProducts([currentProduct]);
+            setShowModal(false);
+          }}
+        >
+          Accept
+        </Button>
+      </div>
+    </Modal>
+  );
+}
