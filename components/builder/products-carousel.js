301975676b29d1b8389026c9fafdf33087bb973d
Marcin Krzemiński <marcin@krzeminski.net>
2024-04-19 17:22:42 +0200

    Extract getting slides to helpers

--- components/builder/products-carousel.js
+++ components/builder/products-carousel.js
@@ -1,7 +1,5 @@
 'use client';
 
-import { Fragment } from 'react';
-
 import clsx from 'clsx';
 import Image from 'next/image';
 
@@ -10,15 +8,11 @@ import { getIcon } from '@lib/icons';
 import Button from '@components/button/button';
 import Carousel from '@components/carousel/carousel';
 
-import { isProductSelected } from './helpers';
+import { getSlides } from './helpers';
 import styles from './products-carousel.module.scss';
 
-const PlusIcon = getIcon('plus');
-const CheckMarkIcon = getIcon('check-mark');
 const CancelIcon = getIcon('cancel');
 const ArrowBackwardIcon = getIcon('arrow-backward');
-const GroupIcon = getIcon('group');
-const UngroupIcon = getIcon('ungroup');
 
 export default function ProductsCarousel({
   className,
@@ -32,108 +26,13 @@ export default function ProductsCarousel({
   toggleGroup,
   toggleProduct,
 }) {
-  const slides = [];
-
-  products?.forEach(group => {
-    group?.variants.forEach((product, index) => {
-      const { isGroup, uteBuilderImages, variantName, variantSlug } = product;
-      const productTitle = variantName;
-      const productImage = uteBuilderImages.imageDesktop?.node?.sourceUrl;
-      const isSelected = isProductSelected(selectedProducts, variantSlug);
-      const isDisabled = disabledProducts.includes(variantSlug);
-      const isGroupItemOpen = isGroup && product.isOpen;
-      const isGroupItemFirst = index === 0;
-      const isGroupItemLast = index === group.variants.length - 1;
-
-      const Icon = (
-        <>
-          {isGroup ? (
-            <>
-              {index === 0 ? (
-                isGroupItemOpen ? (
-                  <UngroupIcon />
-                ) : (
-                  <GroupIcon />
-                )
-              ) : isSelected ? (
-                <CheckMarkIcon />
-              ) : (
-                <PlusIcon />
-              )}
-            </>
-          ) : isSelected ? (
-            <CheckMarkIcon />
-          ) : (
-            <PlusIcon />
-          )}
-        </>
-      );
-
-      const slide = (
-        <Fragment key={index}>
-          <button
-            className={clsx(styles.product, {
-              [styles.isSelected]: isSelected,
-              [styles.isDisabled]:
-                isGroupItemFirst && isGroup && isGroupItemOpen
-                  ? false
-                  : isDisabled,
-              [styles.isGroupItem]: isGroup,
-              [styles.isGroupItemOpen]: isGroupItemOpen,
-              [styles.isGroupItemFirst]: isGroupItemFirst,
-              [styles.isGroupItemLast]: isGroupItemLast,
-            })}
-            onClick={() => {
-              isGroup && index === 0
-                ? toggleGroup(group)
-                : toggleProduct(product);
-            }}
-            type="button"
-          >
-            <div className={styles.productImageContainer}>
-              <Image
-                alt={productTitle}
-                className={styles.productImage}
-                height={Math.round(168 / 1.4)}
-                src={productImage}
-                style={{ objectFit: 'contain' }}
-                width={168}
-              />
-            </div>
-            <div className={styles.productIcon}>{Icon}</div>
-            {productTitle && (
-              <div className={styles.productMeta}>
-                <p className={styles.productName}>{productTitle}</p>
-                {index === 0 && group.minPrice > 0 && (
-                  <span className={styles.productPrice}>
-                    {isGroup && <>Starting from </>}
-                    {new Intl.NumberFormat('en-AU', {
-                      currency: 'AUD',
-                      style: 'currency',
-                    }).format(group.minPrice)}
-                  </span>
-                )}
-
-                {index > 0 && product.price > 0 && (
-                  <span className={styles.productPrice}>
-                    {new Intl.NumberFormat('en-AU', {
-                      currency: 'AUD',
-                      style: 'currency',
-                    }).format(product.price)}
-                  </span>
-                )}
-              </div>
-            )}
-          </button>
-        </Fragment>
-      );
-
-      if (!product.hidden) {
-        slides.push(slide);
-      }
-    });
-  });
-
+  const slides = getSlides(
+    products,
+    selectedProducts,
+    disabledProducts,
+    toggleGroup,
+    toggleProduct,
+  );
   const currentStepTitle =
     stepNumber === 2 && isMobile ? 'Add products to' : stepTitle;
   const productTitle = selectedCover?.variantName;
