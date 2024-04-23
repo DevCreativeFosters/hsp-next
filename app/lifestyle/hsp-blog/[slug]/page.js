5d9063d9061d09bed35e650b68298a1575370ce8
Marcin Krzemiński <marcin@krzeminski.net>
2024-04-23 14:45:00 +0200

    Generate static params for all blog posts

--- app/lifestyle/hsp-blog/[slug]/page.js
+++ app/lifestyle/hsp-blog/[slug]/page.js
@@ -1,5 +1,9 @@
+import { Suspense } from 'react';
+
+import { getAllPostsSlugs } from '@lib/api/get-all-posts-slugs';
 import { getBlogPost } from '@lib/api/get-blog-post';
 import { getRecentBlogPosts } from '@lib/api/get-recent-blog-posts';
+import routes from '@lib/routes';
 
 import { BlogPost } from '@components/blog-post';
 import Layout from '@components/layout/layout';
@@ -18,14 +22,26 @@ export default async function BlogPostPage({ params }) {
 
   return (
     <Layout title={`HSP 4x4 - ${title}`}>
-      <BlogPost
-        content={content}
-        excerpt={excerpt}
-        image={image}
-        relatedPosts={relatedPosts}
-        slug={slug}
-        title={title}
-      />
+      <Suspense fallback={null}>
+        <BlogPost
+          content={content}
+          excerpt={excerpt}
+          image={image}
+          relatedPosts={relatedPosts}
+          slug={slug}
+          title={title}
+        />
+      </Suspense>
     </Layout>
+  );
+}
+
+export async function generateStaticParams() {
+  const posts = await getAllPostsSlugs();
+
+  return (
+    posts.map(post => ({
+      slug: `${routes.lifestyleBlog}/${post.slug}`,
+    })) || []
   );
 }
