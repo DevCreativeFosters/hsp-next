526e08ee5ab7c77cb1007daa6d058733dc0981cb
Marcin Krzemiński <marcin@krzeminski.net>
2024-04-29 16:27:27 +0200

    Get SEO data for product category

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -1,9 +1,22 @@
 import getAllPagesSlugs from '@lib/api/get-all-pages-slugs';
 import { getPageData } from '@lib/api/get-page-data';
+import { getProductCategorySeo } from '@lib/api/get-product-category-seo';
 import { renderBlock } from '@lib/block';
 
 import ContentBlocksPage from '@components/content-blocks-page/content-blocks-page';
 import ProductHeroPage from '@components/product-hero-page/product-hero-page';
+
+export async function generateMetadata({ params }) {
+  if (!params?.slug) {
+    return;
+  }
+
+  const data = await getProductCategorySeo(params.slug);
+
+  return {
+    ...data,
+  };
+}
 
 export default async function DynamicPage({ params }) {
   const slug = params?.slug;
