d390ba57d32b9b666e1076061d26d872d38e1ee3
Marcin Krzemiński <marcin@krzeminski.net>
2024-04-29 17:38:38 +0200

    Remove orphaned code

--- app/[slug]/[makeSlug]/page.js
+++ app/[slug]/[makeSlug]/page.js
@@ -90,16 +90,6 @@ export default async function CategoryPage({ params }) {
   const categoryMakesAndModels = await getCategoriesMakesAndModels();
   const categories = formatCategories(categoryMakesAndModels);
 
-  const slugs = [];
-  categories.forEach(category => {
-    category.makes.forEach(make => {
-      slugs.push({
-        makeSlug: make.slug,
-        slug: category.slug,
-      });
-    });
-  });
-
   return (
     <Layout title="Product">
       <Container>
