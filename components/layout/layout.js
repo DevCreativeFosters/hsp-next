0d8e663bf88e3ebce3d09b5066d7dd1560275d97
Marcin Krzemiński <marcin@krzeminski.net>
2024-05-15 14:44:30 +0200

    Sort products subitems accordingly to menu settings in the backend

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -13,7 +13,7 @@ import { getMenu } from '@lib/api/get-menu';
 import { getMenuDropdownProducts } from '@lib/api/get-menu-dropdown-products';
 import { getProductCategories } from '@lib/api/get-product-categories';
 import { getStores } from '@lib/api/get-stores';
-import { getExcludeTree } from '@lib/helpers';
+import { getExcludeTree, sortMainProductCategories } from '@lib/helpers';
 import normalizeMainMenu from '@lib/normalize-main-menu';
 import normalizeMobileMenu from '@lib/normalize-mobile-menu';
 import normalizeProductData from '@lib/normalize-product-data';
@@ -79,12 +79,17 @@ export default function Layout({
     resources: [],
     services: [],
   };
+
   const footerText = data?.globalOptions?.footerText;
   const topNavigationMenu = normalizeTopNavigationMenu(data?.globalOptions);
   const normalizedMainMenu = normalizeMainMenu(data.mainMenu);
   const normalizedMobileMenu = normalizeMobileMenu(data.mobileMenu);
   const normalizedProductData = normalizeProductData(
     data.mainProductCategories,
+  );
+  const normalizedMainProductCategories = sortMainProductCategories(
+    data.mainProductCategories,
+    normalizedMainMenu,
   );
   const newsletterTitle = data?.globalOptions?.newsletterTitle;
   const newsletterDescription = data?.globalOptions?.newsletterDescription;
@@ -132,7 +137,7 @@ export default function Layout({
         <VehicleProvider>
           <Header
             mainMenu={normalizedMainMenu}
-            mainProductCategories={data.mainProductCategories}
+            mainProductCategories={normalizedMainProductCategories}
             makes={data.makes}
             mobileMenu={normalizedMobileMenu}
             products={normalizedProductData}
