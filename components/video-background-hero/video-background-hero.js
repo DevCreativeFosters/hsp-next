e5e3d36db1a56621b9941a291b772032352b74bb
Marcin Krzemiński <marcin@krzeminski.net>
2024-05-24 12:36:14 +0200

    Allow to use HTML elements for the description field

--- components/video-background-hero/video-background-hero.js
+++ components/video-background-hero/video-background-hero.js
@@ -29,7 +29,10 @@ export default function VideoBackgroundHero({
       <Container>
         <div className={styles.content}>
           <h1 className={styles.title}>{title}</h1>
-          <div className={styles.description}>{description}</div>
+          <div
+            className={styles.description}
+            dangerouslySetInnerHTML={{ __html: description }}
+          />
           {link && (
             <Button href={link.url} rightIcon="play-button" size="large">
               {link.title}
