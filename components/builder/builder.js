f02486a373aefc6290ac3a1900b010e5c71d0486
Marcin Krzemiński <marcin@krzeminski.net>
2024-06-10 19:13:39 +0200

    Show reset button on mobile on every step except 0

--- components/builder/builder.js
+++ components/builder/builder.js
@@ -452,6 +452,12 @@ export default function Builder({
     setShowClashModal(false);
   };
 
+  const handleResetAccept = () => {
+    setSelectedCover(null);
+    setSelectedProducts([]);
+    setStepNumber(0);
+  };
+
   return (
     <>
       {showClashModal && (
@@ -476,15 +482,7 @@ export default function Builder({
               stepNumber={stepNumber}
             />
             {stepNumber > 0 ? (
-              <Preview
-                make={make}
-                model={model}
-                removeProduct={removeProduct}
-                selectedCover={selectedCover}
-                selectedFactoryOption={selectedFactoryOption}
-                selectedProducts={selectedProducts}
-                stepNumber={stepNumber}
-              >
+              <Preview handleResetAccept={handleResetAccept}>
                 {isInlineMapVisible && (
                   <StoreLocatorMap
                     className={styles.map}
@@ -499,9 +497,9 @@ export default function Builder({
           </div>
           <ProductsCarousel
             disabledProducts={disabledProducts}
+            handleResetAccept={handleResetAccept}
             isMobile={isMobile}
             products={stepProducts}
-            stepTitle={stepTitle}
             toggleGroup={toggleGroup}
             toggleProduct={toggleProduct}
           />
