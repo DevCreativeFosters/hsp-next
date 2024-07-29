0a3d04abbaa881b414e798bd3b10d6fb4efe6487
Filip Chrapek <hello@filipchrapek.com>
2024-07-29 16:55:58 +0200

    Add adjustments

--- components/columns-facts/columns-facts.js
+++ components/columns-facts/columns-facts.js
@@ -1,5 +1,6 @@
 'use client';
 
+import clsx from 'clsx';
 import Image from 'next/image';
 
 import { useIsMediumWidth } from '@hooks/useIsMediumWidth';
@@ -13,6 +14,7 @@ import VideoCard from '@components/video-card/video-card';
 import styles from './columns-facts.module.scss';
 
 export default function ColumnsFacts({
+  alignment,
   columns,
   image,
   media,
@@ -23,54 +25,52 @@ export default function ColumnsFacts({
   const isMediumWidth = useIsMediumWidth();
 
   return (
-    <Container collapseMargin>
-      <div className={styles.container}>
-        {title && (
-          <h2
-            className={styles.title}
-            dangerouslySetInnerHTML={{ __html: title }}
+    <Container className={styles.container}>
+      {title && (
+        <h2
+          className={clsx(styles.title, styles[alignment] || styles.left)}
+          dangerouslySetInnerHTML={{ __html: title }}
+        />
+      )}
+
+      <div className={styles.media}>
+        {media === 'image' && image && (
+          <Image
+            alt={image?.node?.altText || ''}
+            className={styles.image}
+            height={isMobile ? 197 : isMediumWidth ? 471 : 942}
+            src={image?.node?.sourceUrl}
+            width={isMobile ? 342 : isMediumWidth ? 660 : 1320}
           />
         )}
+        {media === 'video' && videoFile && (
+          <div className={styles.video}>
+            <VideoCard url={videoFile?.node?.mediaItemUrl} />
+          </div>
+        )}
+      </div>
 
-        <div className={styles.media}>
-          {media === 'image' && image && (
-            <Image
-              alt={image?.node?.altText || ''}
-              className={styles.image}
-              height={isMobile ? 197 : isMediumWidth ? 471 : 942}
-              src={image?.node?.sourceUrl}
-              width={isMobile ? 342 : isMediumWidth ? 660 : 1320}
-            />
-          )}
-          {media === 'video' && videoFile && (
-            <div className={styles.video}>
-              <VideoCard url={videoFile?.node?.mediaItemUrl} />
-            </div>
-          )}
-        </div>
-
-        <div className={styles.columns}>
-          {columns.map((column, index) => (
-            <div className={styles.column} key={index}>
-              {column.description && (
-                <TextElement
-                  className={styles.description}
-                  text={column.description}
-                />
-              )}
-              {column.ctaButton && (
-                <Button
-                  className={styles.button}
-                  href={column.ctaButton.url}
-                  size="large"
-                  target={column.ctaButton?.target || 'self'}
-                >
-                  {column.ctaButton.title}
-                </Button>
-              )}
-            </div>
-          ))}
-        </div>
+      <div className={styles.columns}>
+        {columns.map((column, index) => (
+          <div className={styles.column} key={index}>
+            {column.description && (
+              <TextElement
+                className={styles.description}
+                text={column.description}
+              />
+            )}
+            {column.ctaButton && (
+              <Button
+                className={styles.button}
+                href={column.ctaButton.url}
+                size="large"
+                target={column.ctaButton?.target || 'self'}
+              >
+                {column.ctaButton.title}
+              </Button>
+            )}
+          </div>
+        ))}
       </div>
     </Container>
   );
