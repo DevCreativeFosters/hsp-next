064c00da286e327f911c24d5d5dc095f931da4ea
Jakub Bogucki <jakub.bogucki@xfive.co>
2024-08-28 14:54:00 +0200

    don't generate makes

--- app/[slug]/[makeSlug]/page.js
+++ app/[slug]/[makeSlug]/page.js
@@ -138,20 +138,3 @@ export default async function CategoryPage({ params }) {
     </Layout>
   );
 }
-
-export async function generateStaticParams() {
-  const categoryMakesAndModels = await getCategoriesMakesAndModels();
-  const categories = formatCategories(categoryMakesAndModels);
-
-  const slugs = [];
-  categories.forEach(category => {
-    category.makes.forEach(make => {
-      slugs.push({
-        makeSlug: make.slug,
-        slug: category.slug,
-      });
-    });
-  });
-
-  return slugs;
-}
