31d32111d59f9bb481c30179f152e3845a864b1b
martibelegu <marti.belegu@xfive.co>
2024-09-23 11:17:33 +0200

    HSP-580-setup-sentry

---
+++ sentry.server.config.js
@@ -0,0 +1,17 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+debug: false,
+
+  
+  
+dsn: 'https://2e9ff5c22739fc0f169966eaa19a39fa@o66342.ingest.us.sentry.io/4508001145913344',
+
+  
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+tracesSampleRate: 1,
+});
