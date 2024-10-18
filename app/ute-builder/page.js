0eab25dfe8dc2c5617d78b209bf650f95ff275c4
martibelegu <marti.belegu@xfive.co>
2024-10-18 13:35:09 +0200

    HSP-595 update store locator provider wrap

--- app/ute-builder/page.js
+++ app/ute-builder/page.js
@@ -29,15 +29,15 @@ export default async function UteBuilder() {
   const excludedCategories = await getCategoriesToExclude(globalOptions);
 
   return (
-    <Layout withFooter={false}>
-      <StoreLocatorProvider>
+    <StoreLocatorProvider>
+      <Layout withFooter={false}>
         <UteBuilderPage
           allLocations={allLocations}
           excludedCategories={excludedCategories}
           globalOptions={globalOptions}
           makes={makes}
         />
-      </StoreLocatorProvider>
-    </Layout>
+      </Layout>
+    </StoreLocatorProvider>
   );
 }
