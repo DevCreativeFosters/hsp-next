67b4710eca46cde59408d34562d15ef4f466f72e
Jakub Bogucki <jakub.bogucki@xfive.co>
2025-04-04 14:04:27 +0200

    HSP-707: Update Sticky Accordion: Reserve Space For Vehicle Picker (!696)

--- components/accordion/accordion-trigger.js
+++ components/accordion/accordion-trigger.js
@@ -1,3 +1,5 @@
+import { useCallback, useMemo, useRef, useState } from 'react';
+
 import clsx from 'clsx';
 
 import styles from './accordion-trigger.module.scss';
@@ -12,13 +14,49 @@ export default function AccordionTrigger({
   resetStyling,
   stickyTopOffset,
 }) {
-  const customStyle = {};
-  if (isStickyOnMobile && stickyTopOffset !== undefined) {
-    customStyle.top =
-      typeof stickyTopOffset === 'number'
-        ? `${stickyTopOffset}px`
-        : stickyTopOffset;
-  }
+  const customStyle = useMemo(() => {
+    if (isStickyOnMobile && stickyTopOffset !== undefined) {
+      return {
+        '--accordion-trigger-sticky-top':
+          typeof stickyTopOffset === 'number'
+            ? `${stickyTopOffset}px`
+            : stickyTopOffset,
+      };
+    }
+  }, [isStickyOnMobile, stickyTopOffset]);
+
+  const [isCurrentlySticky, setIsCurrentlySticky] = useState(false);
+
+  const monitorRef = useRef(null);
+  const monitorStateRef = useRef({
+    first: false,
+    second: false,
+  });
+
+  const onMonitorRender = useCallback(el => {
+    if (!el) return;
+
+    if (!monitorRef.current) {
+      monitorRef.current = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          const { dataset } = entry.target;
+          const { type } = dataset;
+
+          monitorStateRef.current[type] = entry.isIntersecting;
+        });
+
+        setIsCurrentlySticky(
+          !monitorStateRef.current.first && monitorStateRef.current.second,
+        );
+      });
+    }
+
+    monitorRef.current.observe(el);
+
+    return () => {
+      monitorRef.current.unobserve(el);
+    };
+  }, []);
 
   return (
     <div
@@ -36,6 +74,25 @@ export default function AccordionTrigger({
       style={customStyle}
       tabIndex={0}
     >
+      {isOpen && isStickyOnMobile && (
+        <>
+          <div
+            className={clsx(styles.monitor, styles.first)}
+            data-type="first"
+            ref={onMonitorRender}
+          />
+          <div
+            className={clsx(styles.monitor, styles.second)}
+            data-type="second"
+            ref={onMonitorRender}
+          />
+          <div
+            className={clsx(styles.belowVehiclePickerBar, {
+              [styles.isCurrentlySticky]: isCurrentlySticky,
+            })}
+          />
+        </>
+      )}
       {children}
     </div>
   );
