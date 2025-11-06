4b63ca98f1facb0dffe8bc71b42a279ad577ba0d
Sapan-Full-Stack-Developer <phptrainee@penguinpeak.com>
2025-11-06 14:36:03 +0530

    Create order status page

--- app/order-status/[orderId]/page.js
+++ app/order-status/[orderId]/page.js
@@ -3,6 +3,11 @@ import { fetchAPI } from '@lib/fetch-api';
 import Container from '@components/container/container';
 import Layout from '@components/layout/layout';
 
+import SuccessIcon from '@assets/icons/check-icon-animate.svg';
+import ErrorIcon from '@assets/icons/error-animate-icon.svg';
+
+import styles from './order.module.scss';
+
 const ORDER_STATUS_MUTATION = `
     mutation checkOrderStatus($orderId: Int!) {
         checkOrderStatusData(input: { orderId: $orderId }) {
@@ -20,7 +25,18 @@ async function page({ params }) {
   return (
     <Layout title="Order Status | HSP">
       <Container>
-        <p>{data?.checkOrderStatusData?.message}</p>
+        <div className={styles.messageContent}>
+          {data?.checkOrderStatusData?.success ? (
+            <div className={styles.successIcon}>
+              <SuccessIcon />
+            </div>
+          ) : (
+            <div className={styles.errorIcon}>
+              <ErrorIcon />
+            </div>
+          )}
+          <h3>{data?.checkOrderStatusData?.message}</h3>
+        </div>
       </Container>
     </Layout>
   );
