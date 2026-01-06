d2ed0b23ee75f8aa470c501e10ccb6eef86008ae
DevCreativeFosters <info@creativefosters.com.au>
2026-01-06 17:33:45 +0530

    Update Product Registration

--- components/account/registered-products/registered-products.js
+++ components/account/registered-products/registered-products.js
@@ -12,25 +12,22 @@ import Button from '@components/button/button';
 
 import styles from './registered-products.module.scss';
 
-const query = `
-    query GetUserRegistrations {
-        productRegistrations(userId: 74) {
-            id
-            first_name
-            last_name
-            product_cate {
-                id
-                name
-                slug
-                url
-                image
-            }
-            serial_number
-            purchase_date
-            store_from
-            created_at
-        }
+const query = /* GraphQL */ `
+  query {
+    userRegisteredProducts {
+      entryId
+      dateCreated
+      status
+      fields
+      productCategory {
+        id
+        name
+        slug
+        url
+        image
+      }
     }
+  }
 `;
 
 function RegisteredProducts() {
@@ -40,9 +37,9 @@ function RegisteredProducts() {
 
   useEffect(() => {
     async function fetchRegisteredProducts() {
-      const variables = { userId: user?.id };
-      const res = await fetchAPI(query, { variables });
-      const data = res?.productRegistrations || [];
+      const res = await fetchAPI(query, { authToken: user?.token });
+      const data = res?.userRegisteredProducts || [];
+
       setRegisteredProducts(data);
     }
     fetchRegisteredProducts();
@@ -62,40 +59,48 @@ function RegisteredProducts() {
       </div>
       <div className={styles.lists}>
         {registeredProducts.length > 0 &&
-          registeredProducts.map((product, index) => (
-            <div className={styles.productBox} key={product?.id}>
-              <figure>
-                <Image
-                  alt={'Product Image'}
-                  className={styles.productImg}
-                  height={93}
-                  src={product?.product_cate?.image}
-                  width={100}
-                />
-              </figure>
-              <div className={styles.info}>
-                <h6>{product?.product_cate?.name}</h6>
-                <div className={styles.desc}>
-                  <div className={styles.left}>
-                    <p>
-                      <strong>Purchase Date:</strong>{' '}
-                      {product?.purchase_date.split('-').reverse().join('/')}
-                    </p>
-                    <p>
-                      <strong>Store Purchased From:</strong>{' '}
-                      {product?.store_from}
-                    </p>
-                  </div>
-                  <div className={styles.right}>
-                    <div className={styles.sNo}>
-                      <p>Serial Number of Unit:</p>
-                      <h4>{product?.serial_number}</h4>
+          registeredProducts.map(product => {
+            const fields = JSON.parse(product?.fields);
+
+            return (
+              <div className={styles.productBox} key={product?.entryId}>
+                <figure>
+                  <Image
+                    alt={'Product Image'}
+                    className={styles.productImg}
+                    height={93}
+                    src={product?.productCategory?.image}
+                    width={100}
+                  />
+                </figure>
+                <div className={styles.info}>
+                  <h6>{fields?.productSubCategories?.value}</h6>
+                  <div className={styles.desc}>
+                    <div className={styles.left}>
+                      <p>
+                        <strong>Purchase Date:</strong>{' '}
+                        {fields?.purchasedate.value
+                          .split(' ')[0]
+                          .split('-')
+                          .reverse()
+                          .join('/')}
+                      </p>
+                      <p>
+                        <strong>Store Purchased From:</strong>{' '}
+                        {fields?.stores.value}
+                      </p>
+                    </div>
+                    <div className={styles.right}>
+                      <div className={styles.sNo}>
+                        <p>Serial Number of Unit:</p>
+                        <h4>{fields?.serialnumberofunit?.value}</h4>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
       </div>
     </div>
   );
