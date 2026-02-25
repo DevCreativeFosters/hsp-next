e1108fa1e1c1f72cad3cf0a59749ccc640353cb6
DevCreativeFosters <info@creativefosters.com.au>
2026-02-25 19:41:06 +0530

    Upgrade design

--- app/(authentication)/forgot-password/page.js
+++ app/(authentication)/forgot-password/page.js
@@ -1,11 +1,9 @@
-import Image from 'next/image';
 import Link from 'next/link';
 
 import Container from '@components/container/container';
 import Layout from '@components/layout/layout';
 
 import Arrow from '@assets/images/arrow.svg';
-import banner from '@assets/images/loginbanner.jpg';
 
 import ClientForgotForm from './client-forgot-form';
 import styles from './forgot-password.module.scss';
@@ -14,16 +12,6 @@ export default function ForgotPassword() {
   return (
     <Layout title="Forgot Password | HSP">
       <section className={styles.accountContent}>
-        <div className={styles.mobilebg}>
-          <figure>
-            <Image
-              alt="Forgot Password Banner"
-              fill
-              src={banner}
-              style={{ objectFit: 'cover' }}
-            />
-          </figure>
-        </div>
         <div className={styles.loginMain}>
           <Container className={styles.loginBG}>
             <div className={styles.loginBGWrap}>
