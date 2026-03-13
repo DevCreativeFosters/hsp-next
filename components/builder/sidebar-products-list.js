bed907381ab9d2cc6e4ad5731934455238e4119a
DevCreativeFosters <info@creativefosters.com.au>
2026-03-13 21:56:52 +0530

    UTE Builder Changes

--- components/builder/sidebar-products-list.js
+++ components/builder/sidebar-products-list.js
@@ -1,9 +1,12 @@
 'use client';
 
 import clsx from 'clsx';
+import Image from 'next/image';
 
 import { formatPrice } from '@lib/helpers';
 import { getIcon } from '@lib/icons';
+
+import PlaceImage from '@assets/images/placeholder-product.jpg';
 
 import styles from './sidebar.module.scss';
 
@@ -20,12 +23,16 @@ export default function ProductsList({ removeProduct, selectedProducts }) {
             return (
               <li className={styles.productsListItem} key={`${slug}_${index}`}>
                 <div className={styles.productBox}>
-                  <span className={styles.productBoxPrice}>
-                    {formatPrice(price)}
-                  </span>
-                  <span className={styles.productBoxName} title={productName}>
-                    {productName}
-                  </span>
+                  <Image alt="Login Banner" src={PlaceImage} />
+                  <div className={styles.infoRight}>
+                    <span className={styles.productBoxName} title={productName}>
+                      {productName}
+                    </span>
+                    <span className={styles.productBoxPrice}>
+                      {formatPrice(price)}
+                      <span>$3,300</span>
+                    </span>
+                  </div>
                 </div>
                 <button
                   className={styles.productRemove}
